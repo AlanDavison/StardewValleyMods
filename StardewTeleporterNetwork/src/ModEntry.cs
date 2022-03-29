14ca362506bea7391a7fc65a35d12696266b3a91
DecidedlyHuman <contact@decidedlyhuman.co.uk>
2022-03-29 13:40:08 +0100

    Initial setup of the project.

---
+++ StardewTeleporterNetwork/src/ModEntry.cs
@@ -0,0 +1,19 @@
+﻿using StardewModdingAPI;
+using StardewTeleporterNetwork.Utilities;
+
+namespace StardewTeleporterNetwork
+{
+	public class ModEntry : Mod
+	{
+		private IModHelper _helper;
+		private IMonitor _monitor;
+		private Logger _logger;
+
+		public override void Entry(IModHelper helper)
+		{
+			_helper = helper;
+			_monitor = Monitor;
+			_logger = new Logger(_monitor);
+		}
+	}
+}
