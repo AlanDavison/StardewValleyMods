894380ee9649f7f35f4440e5279b3f822de4f3e9
DecidedlyHuman <contact@decidedlyhuman.co.uk>
2022-07-08 17:39:46 +0100

    SLN: New mod.

---
+++ SaveLoadedNotifier/src/ModEntry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace SaveLoadedNotifier
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            // It helps to initialise the i18n stuff.
+            I18n.Init(helper.Translation);
+            
+            helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+        }
+
+        [EventPriority((EventPriority)int.MinValue)]
+        private void OnSaveLoaded(object? sender, SaveLoadedEventArgs e)
+        {
+            try
+            {
+                // This will throw if the cue doesn't exist.
+                Game1.soundBank.GetCueDefinition("whistle");
+            }
+            catch (Exception ex)
+            {
+                Monitor.Log("The \"whistle\" sound cue doesn't appear to exist.");
+            }
+                
+            // If we've reached here, we're fine to play the sound, and display our dialogue.
+            Game1.soundBank.PlayCue("whistle");
+            Game1.drawDialogueNoTyping(I18n.IntoTheGame_SaveLoaded());
+        }
+    }
+}
