56f3c4c1bd0ca678bf30580e01f36967e742b7c5
DecidedlyHuman <contact@decidedlyhuman.co.uk>
2022-07-21 15:13:47 +0100

    CYP: The mod exists!

---
+++ CarryYourPet/src/CarriedCharacter.cs
@@ -0,0 +1,39 @@
+﻿using StardewValley;
+using StardewValley.Characters;
+
+namespace CarryYourPet
+{
+    public class CarriedCharacter
+    {
+        private NPC npc;
+        private bool shouldDraw;
+        // private bool isPet;
+
+        // public bool IsPet
+        // {
+        //     get => isPet;
+        //     set
+        //     {
+        //         isPet = value;
+        //     }
+        // }
+        
+        public bool ShouldDraw
+        {
+            get => shouldDraw;
+            set
+            {
+                shouldDraw = value;
+            }
+        }
+        
+        public NPC Npc
+        {
+            get => npc;
+            set
+            {
+                npc = value;
+            }
+        }
+    }
+}
---
+++ CarryYourPet/src/ModConfig.cs
@@ -0,0 +1,10 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Utilities;
+
+namespace CarryYourPet
+{
+	public class ModConfig
+    {
+        public KeybindList HoldToCarryNpc = KeybindList.Parse("LeftShift");
+    }
+}
