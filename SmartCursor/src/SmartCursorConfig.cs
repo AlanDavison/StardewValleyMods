3aa15306824ee63402733d7ff5cc6f36b42628dd
DecidedlyHuman <contact@decidedlyhuman.co.uk>
2022-08-05 21:52:09 +0100

    SC: 1.0.0 release.

---
+++ SmartCursor/src/SmartCursorConfig.cs
@@ -0,0 +1,15 @@
+﻿using StardewModdingAPI;
+
+namespace SmartCursor
+{
+    public class SmartCursorConfig
+    {
+        // Keybinds
+        public SButton SmartCursorHold = SButton.LeftControl;
+
+        // Toggles
+        public bool AllowTargetingBabyTrees = false;
+        public bool AllowTargetingGiantCrops = false;
+        public bool AllowTargetingTappedTrees = false;
+    }
+}
