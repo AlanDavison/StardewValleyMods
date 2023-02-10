6b60fc8dfb8f8acfa36a84e77f950fce777bb3c3
Alan <contact@decidedlyhuman.co.uk>
2023-02-10 20:14:57 +0000

    RPGB: Initial commit for a future WIP.

---
+++ RPGBatles/src/Framework/BattlerEnemy.cs
@@ -0,0 +1,14 @@
+﻿namespace RPGBattles.Framework;
+
+public class BattlerEnemy
+{
+    private int healthPoints;
+    private int staminaPoints;
+    private int magicPoints;
+    private BattlerSprite sprite;
+
+    public BattlerEnemy()
+    {
+
+    }
+}
---
+++ RPGBatles/src/ModEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using StardewModdingAPI;
+
+namespace RPGBattles;
+
+public class ModEntry : Mod
+{
+    public override void Entry(IModHelper helper)
+    {
+        throw new NotImplementedException();
+    }
+}
