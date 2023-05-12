c3322adabd2f0cb3261432e88d585d44da3365d0
Alan <contact@decidedlyhuman.co.uk>
2023-05-12 15:15:55 +0100

    VAF: Incremental commit of everything so far.

---
+++ VAFPackFormat/src/Models/VoRoot.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace VAFPackFormat.Models;
+
+public class VoRoot
+{
+    public string Format { get; set; }
+    public List<string> Characters { get; set; }
+}
