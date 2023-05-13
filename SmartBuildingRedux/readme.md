64472cab557c28147968e6d8be378eeb27cd17fa
Alan <contact@decidedlyhuman.co.uk>
2023-05-13 18:20:40 +0100

    SBR: Initial commit.

---
+++ SmartBuildingRedux/readme.md
@@ -0,0 +1,9 @@
+﻿# Smart Building Redux
+
+All releases can be found on my [Nexus page](https://www.nexusmods.com/users/79440738?tab=user+files), and (if I
+remember to auto-sync them), my [ModDrop](https://www.moddrop.com/stardew-valley/profile/251772/mods) page.
+
+### What it does
+
+### Extra information
+
---
+++ SmartBuildingRedux/src/ModEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DecidedlyShared.Logging;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+
+namespace SmartBuildingRedux;
+
+public class ModEntry : Mod
+{
+    private Logger logger;
+
+    public override void Entry(IModHelper helper)
+    {
+        this.logger = new Logger(this.Monitor, helper.Translation);
+
+        helper.Events.Input.ButtonPressed += this.InputOnButtonPressed;
+    }
+
+    private void InputOnButtonPressed(object? sender, ButtonPressedEventArgs e)
+    {
+
+    }
+}
