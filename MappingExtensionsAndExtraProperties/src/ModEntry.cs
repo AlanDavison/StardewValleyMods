be232da12b6d379e692f7866fec49a9e5e09d9ce
Alan <contact@decidedlyhuman.co.uk>
2023-05-22 15:30:51 +0100

    MEEP: Quickly commented out background stuff. No time to fix properly yet.

--- MappingExtensionsAndExtraProperties/src/ModEntry.cs
+++ MappingExtensionsAndExtraProperties/src/ModEntry.cs
@@ -98,28 +98,28 @@ public class ModEntry : Mod
         helper.Events.Player.Warped += (sender, args) =>
         {
             // TODO: Finish MEEP background functionality.
-            string image = args.NewLocation.getMapProperty("MEEP_Background_Image");
-            string tile = args.NewLocation.getMapProperty("MEEP_Background_Tile_Size");
-            string variation = args.NewLocation.getMapProperty("MEEP_Background_Tile_Variation");
-
-            int mapWidth = args.NewLocation.map.DisplayWidth;
-            int mapHeight = args.NewLocation.map.DisplayHeight;
-
-            // I want all of these for this test.
-            if (image is null || tile is null || variation is null)
-                return;
-
-            bool imageParsed = Parsers.TryParse(image, out MapBackgroundImage backgroundImage);
-            bool sizeParsed = Parsers.TryParse(tile, out MapBackgroundTileSize tileSize);
-            bool variationParsed = Parsers.TryParse(variation, out MapBackgroundTileVariation tileVariation);
-
-            if (!imageParsed || !sizeParsed || !variationParsed)
-                return;
-
-            Texture2D texture = Game1.content.Load<Texture2D>(backgroundImage.ImagePath);
-            int numTiles = (texture.Width / tileSize.Width) * (texture.Height / tileSize.Height);
-            Game1.background = new Background(texture, 1, mapWidth / tileSize.Width, mapHeight / tileSize.Height,
-                tileSize.Width, tileSize.Height, 4f, 1, numTiles, 1d, Color.White);
+            // string image = args.NewLocation.getMapProperty("MEEP_Background_Image");
+            // string tile = args.NewLocation.getMapProperty("MEEP_Background_Tile_Size");
+            // string variation = args.NewLocation.getMapProperty("MEEP_Background_Tile_Variation");
+            //
+            // int mapWidth = args.NewLocation.map.DisplayWidth;
+            // int mapHeight = args.NewLocation.map.DisplayHeight;
+            //
+            // // I want all of these for this test.
+            // if (image is null || tile is null || variation is null)
+            //     return;
+            //
+            // bool imageParsed = Parsers.TryParse(image, out MapBackgroundImage backgroundImage);
+            // bool sizeParsed = Parsers.TryParse(tile, out MapBackgroundTileSize tileSize);
+            // bool variationParsed = Parsers.TryParse(variation, out MapBackgroundTileVariation tileVariation);
+            //
+            // if (!imageParsed || !sizeParsed || !variationParsed)
+            //     return;
+            //
+            // Texture2D texture = Game1.content.Load<Texture2D>(backgroundImage.ImagePath);
+            // int numTiles = (texture.Width / tileSize.Width) * (texture.Height / tileSize.Height);
+            // Game1.background = new Background(texture, 1, mapWidth / tileSize.Width, mapHeight / tileSize.Height,
+            //     tileSize.Width, tileSize.Height, 4f, 1, numTiles, 1d, Color.White);
         };
 
         helper.Events.Input.ButtonPressed += (sender, args) =>
