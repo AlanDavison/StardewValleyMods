d56cc5f729ab18e6d69e92af2729b3e54a2c5006
Alan <contact@decidedlyhuman.co.uk>
2023-06-13 09:42:24 +0100

    SLN: Normalised the state of this branch.

--- VAFPackFormat/src/Models/VoRoot.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace VAFPackFormat.Models;
-
-public class VoRoot
-{
-    public string Format { get; set; }
-    public List<string> Characters { get; set; }
-}
