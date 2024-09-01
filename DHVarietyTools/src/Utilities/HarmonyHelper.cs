29811ab01fa25dbb9002da8c081d140e2d9f1e3f
Alan Davison <contact@decidedlyhuman.co.uk>
2024-09-01 19:20:12 +0100

    DVT: Re-add HarmonyHelper to DVT mod.

---
+++ DHVarietyTools/src/Utilities/HarmonyHelper.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Reflection;
+using DecidedlyShared.Models;
+using HarmonyLib;
+using StardewValley;
+
+namespace DHVarietyTools.Utilities;
+
+public class HarmonyHelper
+{
+    public static DescriptiveBool TryPrefixPatchMethod(Harmony harmony, Type patchesClass, string methodName)
+    {
+        try
+        {
+            MethodInfo? prefixMethod = patchesClass.GetMethod(methodName);
+
+            if (prefixMethod is not null)
+            {
+                harmony.Patch(
+                    AccessTools.Method(typeof(Tool), nameof(Tool.DoFunction)),
+                    prefix: new HarmonyMethod(prefixMethod));
+
+                return new DescriptiveBool(true);
+            }
+        }
+        catch (Exception e) when (e is ArgumentNullException || e is AmbiguousMatchException)
+        {
+            return new DescriptiveBool(false, methodName, "Caught exception.", e);
+        }
+
+        return new DescriptiveBool(false, methodName, $"Unknown error patching method {methodName} in class {patchesClass.AssemblyQualifiedName}");
+    }
+
+    public static DescriptiveBool TryPostfixPatchMethod(Harmony harmony, Type patchesClass, string methodName)
+    {
+        try
+        {
+            MethodInfo? prefixMethod = patchesClass.GetMethod(methodName);
+
+            if (prefixMethod is not null)
+            {
+                harmony.Patch(
+                    AccessTools.Method(typeof(Tool), nameof(Tool.DoFunction)),
+                    postfix: new HarmonyMethod(prefixMethod));
+
+                return new DescriptiveBool(true);
+            }
+        }
+        catch (Exception e) when (e is ArgumentNullException || e is AmbiguousMatchException)
+        {
+            return new DescriptiveBool(false, methodName, "Caught exception.", e);
+        }
+
+        return new DescriptiveBool(false, methodName, $"Unknown error patching method {methodName} in class {patchesClass.AssemblyQualifiedName}");
+    }
+}
