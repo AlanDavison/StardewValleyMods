5ebbca3bfc2bd6e6107d63d69863a1234b3ff44c
Alan Davison <contact@decidedlyhuman.co.uk>
2024-09-09 07:26:04 +0100

    DVT: Switch to constructor for DescriptiveBool. Primary constructor was causing very strange issues for some reason.

--- DecidedlyShared/Models/DescriptiveBool.cs
+++ DecidedlyShared/Models/DescriptiveBool.cs
@@ -2,12 +2,20 @@ using System;
 
 namespace DHVarietyTools.Utilities;
 
-public class DescriptiveBool(bool state, string context = "", string message = "", Exception? exception = null)
+public class DescriptiveBool
 {
     private bool state = default;
     private string message;
     private string context;
     private Exception? exception;
+
+    public DescriptiveBool(bool state, string context = "", string message = "", Exception? exception = null)
+    {
+        this.state = state;
+        this.context = context;
+        this.message = message;
+        this.exception = exception;
+    }
 
     public Exception? Exception => this.exception;
     public string Context => this.context;
