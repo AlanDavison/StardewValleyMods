b8b0ed689adc265c9d101c77892ff09a2ab4a990
Alan Davison <contact@decidedlyhuman.co.uk>
2024-11-04 14:05:55 +0000

    MEEP: Add input sanitisation to event command.

--- MappingExtensionsAndExtraProperties/src/Functionality/EventCommands.cs
+++ MappingExtensionsAndExtraProperties/src/Functionality/EventCommands.cs
@@ -1,4 +1,5 @@
-﻿using DecidedlyShared.Logging;
+﻿using System;
+using DecidedlyShared.Logging;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
@@ -35,24 +36,24 @@ public class EventCommands
     public static void AddColouredSlime(Event e, string[] args, EventContext context)
     {
         if (!ArgUtility.TryGetVector2(args, 1, out Vector2 tile, out string? error, integerOnly: false, "Vector2 tile") ||
-            !ArgUtility.TryGetDirection(args, 3, out int facingDirection, out error, "int facingDirection") ||
-            !ArgUtility.TryGetInt(args, 4, out int red, out error, "int facingDirection") ||
-            !ArgUtility.TryGetInt(args, 5, out int green, out error, "int facingDirection") ||
-            !ArgUtility.TryGetInt(args, 6, out int blue, out error, "int facingDirection"))
+            !ArgUtility.TryGetInt(args, 3, out int red, out error, "int red") ||
+            !ArgUtility.TryGetInt(args, 4, out int green, out error, "int green") ||
+            !ArgUtility.TryGetInt(args, 5, out int blue, out error, "int blue"))
         {
             context.LogErrorAndSkip(error);
             return;
         }
 
+        red = Math.Clamp(red, 0, 255);
+        green = Math.Clamp(green, 0, 255);
+        blue = Math.Clamp(blue, 0, 255);
+
         string slimeTexture = "Characters\\Monsters\\Green Slime";
         int slimeSpriteWidth = 16;
         int slimeSpriteHeight = 24;
 
-        AnimatedSprite slimeSprite = new AnimatedSprite(slimeTexture, 0, slimeSpriteWidth, slimeSpriteHeight);
-        NPC slime = new NPC(slimeSprite, tile * 64f, facingDirection, "Slime", Game1.content);
-
-        // GreenSlime slime = new GreenSlime(tile * 64f, new Color(red, green, blue));
-        // slime.timeSinceLastJump = -1000000;
+        GreenSlime slime = new GreenSlime(tile * 64f, new Color(red, green, blue));
+        slime.timeSinceLastJump = 0;
 
         e.actors.Add(slime);
         e.currentCommand++;
