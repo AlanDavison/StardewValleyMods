da8626a03603c08b3fa4ff2b53fd28bae5d5cccd
Alan <contact@decidedlyhuman.co.uk>
2025-02-20 12:32:55 +0000

    MB: Initial commit of skeleton for Modder Bob mod.

---
+++ ModderBob/src/ModEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using StardewModdingAPI;
+
+namespace ModderBob;
+
+public class ModEntry : Mod
+{
+    public override void Entry(IModHelper helper)
+    {
+        throw new NotImplementedException();
+    }
+}
