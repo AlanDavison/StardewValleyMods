92d5fbb1b5eaeb93f496ff5983144cabd1f751ba
Alan <contact@decidedlyhuman.co.uk>
2025-05-19 15:27:49 +0100

    GP: Add glowing path items, flooring, and recipes.

---
+++ GlowingPaths/release-notes.md
@@ -0,0 +1,9 @@
+﻿# Glowing Paths Changelog
+
+## x.x.x
+#### Fixes
+* Fixed a thing.
+#### Tweaks
+* Tweaked a thing.
+#### Additions
+* Added a thing.
---
+++ GlowingPaths/src/ModEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using StardewModdingAPI;
+
+namespace GlowingPaths;
+
+public class ModEntry : Mod
+{
+    public override void Entry(IModHelper helper)
+    {
+
+    }
+}
