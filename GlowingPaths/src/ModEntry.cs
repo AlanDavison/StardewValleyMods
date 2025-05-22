19e85d9e9ca4bcab9ba8d8f8763939ad15e76f5b
Alan <contact@decidedlyhuman.co.uk>
2025-05-22 12:11:37 +0100

    Starting point commit.

--- GlowingPaths/src/ModEntry.cs
+++ GlowingPaths/src/ModEntry.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using HarmonyLib;
 using StardewModdingAPI;
 
 namespace GlowingPaths;
@@ -7,6 +7,17 @@ public class ModEntry : Mod
 {
     public override void Entry(IModHelper helper)
     {
+        var harmony = new Harmony(this.ModManifest.UniqueID);
 
+        harmony.Patch(
+                AccessTools.Method(typeof(SObject), nameof(SObject.placementAction)),
+                prefix: new HarmonyMethod(typeof(Patches), nameof(PlacementAction_Prefix)));
+    }
+
+    public static bool PlacementAction_Prefix(SObject __instance, GameLocation location, int x, int y, Farmer who)
+    {
+
+
+        return true;
     }
 }
