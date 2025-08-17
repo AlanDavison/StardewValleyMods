27036fb5b5eecec39eb5ccb0bf3983dccc61623c
Alan <contact@decidedlyhuman.co.uk>
2025-08-17 10:15:59 +0100

    EJF: Add rough version of log-respawning. Consider using map data instead of needing to iterate through all tiles.

--- ExJojaFarm/src/ModEntry.cs
+++ ExJojaFarm/src/ModEntry.cs
@@ -1,23 +1,27 @@
-﻿using DecidedlyShared.Logging;
+﻿using System;
+using DecidedlyShared.Logging;
 using HarmonyLib;
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewValley;
 using StardewValley.Buildings;
 using StardewValley.TerrainFeatures;
+using xTile.Tiles;
 using Utility = ExJojaFarm.Utilities.Utility;
 
 namespace ExJojaFarm;
 
 public class ModEntry : Mod
 {
-    private const string FARM_ID = "DH.ExJojaFarm.ExJojaFarmContent_Farm";
+    public const string FARM_ID = "DH.ExJojaFarm.ExJojaFarmContent_Farm";
     private Logger logger;
     public static Logger StaticLogger;
+    private static IModHelper Helper;
 
     public override void Entry(IModHelper helper)
     {
         this.logger = new Logger(this.Monitor);
+        ModEntry.Helper = helper;
         ModEntry.StaticLogger = this.logger;
         Harmony harmony = new Harmony(this.ModManifest.UniqueID);
 
@@ -27,14 +31,74 @@ public class ModEntry : Mod
                 nameof(ModEntry.Farm_OnNewGame_Postfix)));
 
         harmony.Patch(
+            original: AccessTools.Method(typeof(Farm), nameof(Farm.DayUpdate)),
+            postfix: new HarmonyMethod(typeof(ModEntry),
+                nameof(ModEntry.Farm_DayUpdate_Postfix)));
+
+        harmony.Patch(
             original: AccessTools.Method(typeof(Tree), nameof(Tree.dayUpdate)),
             postfix: new HarmonyMethod(typeof(ModEntry),
                 nameof(ModEntry.Tree_DayUpdate_Postfix)));
     }
 
+    public static void Farm_DayUpdate_Postfix(Farm __instance, int dayOfMonth)
+    {
+        if (!Utility.IsExJojaFarm())
+            return;
+
+        int mapWidth = __instance.Map.GetLayer("Paths").Tiles.Array.GetLength(0);
+        int mapHeight = __instance.Map.GetLayer("Paths").Tiles.Array.GetLength(1);
+
+        if (mapWidth == 0 || mapHeight == 0)
+            return;
+
+        StaticLogger.Log("Doing DayUpdate for farm.", LogLevel.Info);
+
+        for (int x = 0; x < mapWidth; x++)
+        {
+            for (int y = 0; y < mapHeight; y++)
+            {
+                Tile tile;
+
+                try
+                {
+                    tile = __instance.Map.GetLayer("Paths").Tiles.Array[x, y];
+                }
+                catch (Exception e)
+                {
+                    StaticLogger.Warn($"Caught exception getting tile at {x},{y} on Paths layer.");
+
+                    continue;
+                }
+
+                StaticLogger.Log("Doing safety check before accessing tile properties.", LogLevel.Info);
+
+                if (tile is null)
+                    continue;
+
+                if (tile.Properties is null || !tile.Properties.ContainsKey("DH_Regrow_Log"))
+                    continue;
+
+                StaticLogger.Log("Trying to spawn log!", LogLevel.Info);
+                Vector2 logTile = new Vector2(x, y);
+
+                if (__instance.CanItemBePlacedHere(logTile) &&
+                    __instance.CanItemBePlacedHere(logTile + new Vector2(1, 0)) &&
+                    __instance.CanItemBePlacedHere(logTile + new Vector2(1, 1)) &&
+                    __instance.CanItemBePlacedHere(logTile + new Vector2(0, 1)))
+                {
+                    __instance.resourceClumps.Add(new ResourceClump(602, 2, 2, logTile));
+                }
+            }
+        }
+    }
+
     public static void Tree_DayUpdate_Postfix(Tree __instance)
     {
         if (__instance.Location is not Farm)
+            return;
+
+        if (!Utility.IsExJojaFarm())
             return;
 
         if (__instance.growthStage.Value < 5) // 5 is fully grown.
