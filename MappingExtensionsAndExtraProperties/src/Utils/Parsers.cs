29596d431f05d3ce411db10c8beafd3fb28a79b6
Alan <contact@decidedlyhuman.co.uk>
2025-12-07 14:35:20 +0000

    MEEP: Remove old debug log spam.

--- MappingExtensionsAndExtraProperties/src/Utils/Parsers.cs
+++ MappingExtensionsAndExtraProperties/src/Utils/Parsers.cs
@@ -88,8 +88,6 @@ public class Parsers
     public static bool TryParseIncludingKey(string property, out CloseupInteractionText parsedProperty)
     {
         parsedProperty = new CloseupInteractionText();
-        logger.Log($"Parsing text property: {property}", LogLevel.Info);
-
         string[] splitProperty = property.Split(" ");
 
         if (splitProperty.Length < 2)
