e431120198bb3f913f707a052aa1223d9089c1fa
Alan <contact@decidedlyhuman.co.uk>
2026-03-24 19:02:06 +0000

    ZPM: Fix zoom oddities with events. (#75)

--- ZoomPerMap/src/ModEntry.cs
+++ ZoomPerMap/src/ModEntry.cs
@@ -1,5 +1,7 @@
-﻿using DecidedlyShared.APIs;
+﻿using System;
+using DecidedlyShared.APIs;
 using DecidedlyShared.Logging;
+using HarmonyLib;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
@@ -11,22 +13,30 @@ namespace ZoomPerMap;
 public class ModEntry : Mod
 {
     private Logger logger;
+    private static Logger StaticLogger;
     private ModConfig config;
+    private static ModConfig StaticConfig;
 
     public override void Entry(IModHelper helper)
     {
         I18n.Init(helper.Translation);
         this.config = helper.ReadConfig<ModConfig>();
+        StaticConfig = this.config;
         this.logger = new Logger(this.Monitor);
         helper.Events.Display.MenuChanged += this.DisplayOnMenuChanged;
         helper.Events.Player.Warped += this.PlayerOnWarped;
         helper.Events.GameLoop.GameLaunched += this.GameLoopOnGameLaunched;
         helper.Events.GameLoop.SaveLoaded += this.GameLoopOnSaveLoaded;
+
+        Harmony harmony = new Harmony(this.ModManifest.UniqueID);
+        harmony.Patch(
+            AccessTools.Method(typeof(Event), nameof(Event.exitEvent)),
+            postfix: new HarmonyMethod(typeof(ModEntry), nameof(ModEntry.ExitEvent_Postfix)));
     }
 
     private void GameLoopOnSaveLoaded(object? sender, SaveLoadedEventArgs e)
     {
-        Game1.options.desiredBaseZoomLevel = this.GetZoomForLocation(Game1.currentLocation);
+        Game1.options.desiredBaseZoomLevel = GetZoomForLocation(Game1.currentLocation);
     }
 
     private void GameLoopOnGameLaunched(object? sender, GameLaunchedEventArgs e)
@@ -65,9 +75,29 @@ public class ModEntry : Mod
         }
     }
 
+    public static void ExitEvent_Postfix(Event __instance)
+    {
+        try
+        {
+            Game1.options.desiredBaseZoomLevel = GetZoomForLocation(Game1.player.currentLocation);
+        }
+        catch (Exception e)
+        {
+            StaticLogger.Error("Caught an exception in Event.exitMethod postfix.");
+
+            StaticLogger.Exception(e);
+        }
+    }
+
     private void PlayerOnWarped(object? sender, WarpedEventArgs e)
     {
-        Game1.options.desiredBaseZoomLevel = this.GetZoomForLocation(e.NewLocation);
+        if (Game1.eventUp)
+        {
+            Game1.options.desiredBaseZoomLevel = 1f;
+            return;
+        }
+
+        Game1.options.desiredBaseZoomLevel = GetZoomForLocation(e.NewLocation);
     }
 
     private void DisplayOnMenuChanged(object? sender, MenuChangedEventArgs e)
@@ -96,16 +126,16 @@ public class ModEntry : Mod
         }
     }
 
-    private float GetZoomForLocation(GameLocation location)
+    private static float GetZoomForLocation(GameLocation location)
     {
-        if (this.config.zoomLevels.TryGetValue(location.Name, out float zoomLevel))
+        if (StaticConfig.zoomLevels.TryGetValue(location.Name, out float zoomLevel))
         {
             return zoomLevel;
         }
 
         if (location is MineShaft || location is VolcanoDungeon)
-            return this.config.defaultMineZoomLevel;
+            return StaticConfig.defaultMineZoomLevel;
 
-        return location.IsOutdoors ? this.config.defaultOutdoorZoomLevel : this.config.defaultIndoorZoomLevel;
+        return location.IsOutdoors ? StaticConfig.defaultOutdoorZoomLevel : StaticConfig.defaultIndoorZoomLevel;
     }
 }
